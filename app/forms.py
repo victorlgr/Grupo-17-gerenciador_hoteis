003a6b37af35da96cbecd7ec0dc278d1062e90be
Clayton Oliveira <claytonoliveira@MacBook-Pro-2.local>
2022-03-20 10:51:10 -0300

    add folder structe to static files, add a example LP and some style for the project

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms  import StringField, TextAreaField, DateTimeField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, EqualTo, DataRequired
 
 class CreateUserForm(FlaskForm):
 	name = StringField(u'Nome', validators = [InputRequired()])
 	email = StringField(u'E-mail', validators = [InputRequired()])
 	password = PasswordField(u'Senha', validators = [InputRequired()])
-	password_confirmation = PasswordField(u'Confirmação de Senha', validators = [InputRequired()])
+	password_confirmation = PasswordField(u'Confirmação de Senha', validators = [InputRequired(), DataRequired(), EqualTo('password')])
 	# date = DateTimeField(u'Data', format='%d/%m/%Y %H:%M')
 	#recaptcha = RecaptchaField(u'Recaptcha')
-
 class LoginForm(FlaskForm):
 	email = StringField(u'Email', validators = [InputRequired()])
 	password = PasswordField(u'Senha', validators = [InputRequired()])
